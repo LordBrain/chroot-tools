8765df33854a8ab1fba65d195b54daccfa1af1fb
James Crayne <jim.crayne@gmail.com>
2014-04-17 22:46:28 -0400

    usage message, improved

--- chroot-mtab.hs
+++ chroot-mtab.hs
@@ -11,6 +11,11 @@ usage = putStrLn $
 --            "\n   schrootname - name of schroot (assume debian locations)" ++
 --            "\n                 Presently, this is ignored if 3 parameters are given." ++
             "Usage: ./chroot-mtab fstabfile mtabfile" ++
+            "\n" ++
+            "\n   copies /proc/mounts to the specified mtabfile, but filters out" ++
+            "\n   directories that are not listed in the specified fstabfile." ++
+            "\n" ++
+            "\n Command-line Arguments: " ++
             "\n   fstabfile - name of schroot fstab file (in it's profile dir)" ++
             "\n   mtabfile - where to copy the mtab too"
 
@@ -20,7 +25,8 @@ main = do
     let [fstabfilename , mtabfile] = args
     fstab <- fmap (filter ("/" `isPrefixOf`) . lines) $ readFile fstabfilename
     mounts <-  fmap lines $ readFile "/proc/mounts"
-    let new_mtab = nub [ x | x <- mounts, y <- fstab, ((==) `on` (take 1 . drop 1) ) x y]
+    let new_mtab = map (take 1 . drop 1 . words) fstab >>= \word -> filter ((== word) . take 1 . drop 1 . words) mounts
+--nub [ x | x <- mounts, y <- fstab, ((==) `on` (take 1 . drop 1) ) x y]
     writeFile mtabfile  (unlines new_mtab)
     
     
