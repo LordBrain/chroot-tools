ae73fde731dad8a7f76d09f0feef9bcb6f794321
James Crayne <jim.crayne@gmail.com>
2014-04-17 22:28:42 -0400

    changed interface, specify filenames only

--- chroot-mtab.hs
+++ chroot-mtab.hs
@@ -7,27 +7,20 @@ import Data.Function
 import Data.List
 
 usage = putStrLn $
-            "Usage: ./chroot-mtab schrootname fstabfile schrootdir" ++
-            "\n   schrootname - name of schroot" ++
+--            "Usage: ./chroot-mtab schrootname [fstabfile] [mtabfile]" ++
+--            "\n   schrootname - name of schroot (assume debian locations)" ++
+--            "\n                 Presently, this is ignored if 3 parameters are given." ++
+            "Usage: ./chroot-mtab fstabfile mtabfile" ++
             "\n   fstabfile - name of schroot fstab file (in it's profile dir)" ++
-            "\n   schrootdir - where to copy the mtab too"
-safeSecond = (!!1)
+            "\n   mtabfile - where to copy the mtab too"
 
-data Mounts a = MOUNTS [a] deriving Show
-data FSTAB a = FSTAB [a] deriving Show
 main = do
     args <- getArgs
-    when (length args < 3) $ usage >> exitFailure 
-    let [schrtname , fstabfilename , schrootdir] = args
-    putStrLn "Working.."
-    fstab <- fmap (filter ("/" `isPrefixOf`)) $ fmap lines $ readFile fstabfilename
-    mapM print $ map (FSTAB . words) fstab
-    --mounts <- --fmap (filter ("/" `isPrefixOf`). map (unwords . drop 1 . words)) $ fmap lines $ readFile "/proc/mounts"
+    when (length args /= 2) $ usage >> exitFailure 
+    let [schrtname , fstabfilename , mtabfile] = args
+    fstab <- fmap (filter ("/" `isPrefixOf`) . lines) $ readFile fstabfilename
     mounts <-  fmap lines $ readFile "/proc/mounts"
-    mapM print $ map (MOUNTS . words) mounts
-    let new_mtab = nub [ x | x <- mounts, y <- fstab, ((==) `on` take 1 ) x y]
-    print new_mtab
---flip concatMap fstab (\(words -> (head) -> dirname) -> filter (not . null . dropWhile (/= dirname) . words) mounts)
-    writeFile (schrootdir ++ "/mtab") (unlines new_mtab)
+    let new_mtab = nub [ x | x <- mounts, y <- fstab, ((==) `on` (take 1 . drop 1) ) x y]
+    writeFile mtabfile  (unlines new_mtab)
     
     
